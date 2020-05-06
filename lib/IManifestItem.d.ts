51ee30585dfb02055f649a4fdc79fd198f218fb5
Norman Breau <norman@normanbreau.com>
2020-05-06 00:45:14 -0300

    Added type to ManifestItem interface

--- lib/IManifestItem.d.ts
+++ lib/IManifestItem.d.ts
@@ -1,4 +1,16 @@
 export interface IManifestItem {
+    /**
+     * The starting byte position, or the byte offset
+     */
     start: number;
+    /**
+     * The ending byte
+     */
     end: number;
+    /**
+     * The mime type. Uses `mime-types`
+     * to determine the mime type based on extension.
+     * If not known, defaults to `"application/octet-stream"`
+     */
+    type: string;
 }
--- src/IManifestItem.ts
+++ src/IManifestItem.ts
@@ -1,5 +1,20 @@
 
 export interface IManifestItem {
+    
+    /**
+     * The starting byte position, or the byte offset
+     */
     start: number;
+
+    /**
+     * The ending byte
+     */
     end: number;
+
+    /**
+     * The mime type. Uses `mime-types` 
+     * to determine the mime type based on extension.
+     * If not known, defaults to `"application/octet-stream"`
+     */
+    type: string;
 }
